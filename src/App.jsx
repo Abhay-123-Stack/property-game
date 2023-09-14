d3970149e10b19260f3f51f9919470e0d75eaaba
Sayantan <sp@unthread.world>
2023-09-15 00:31:53 +0530

    onRent implemented

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,8 @@ function App() {
   const [inc, setInc] = useState(1.05);
   const [intId, setIntId] = useState(-1);
   const [auto, setAuto] = useState(false);
-  const [tmp, setTmp] = useState(0.1);
-  useEffect(() => {
-    return () => {
-      second;
-    };
-  }, [third]);
+  const [tmp, setTmp] = useState(0);
+  const [isRented, setIsRented] = useState(false);
 
   const onBuy = () => {
     setLevel(level + 1);
@@ -24,32 +20,61 @@ function App() {
     setInc(inc + 0.05);
   };
 
-  const onRent = () => {};
+  const onRent = () => {
+    setIsRented(true);
+  };
+  useEffect(() => {
+    if (isRented) {
+      const iid = setInterval(() => {
+        setTmp((prevTmp) => {
+          if (prevTmp > time) {
+            setIsRented(false);
+            return 0;
+          }
+          return prevTmp + 0.1;
+        });
+      }, 100);
+      return () => clearInterval(iid);
+    }
+  }, [isRented, time]);
 
   const onManage = () => {};
 
   return (
     <>
       <figure>
-        <img src="asset/01.png" alt="" width="32px" />
+        <img
+          src="asset/01.png"
+          alt="Normal House"
+        />
         <figcaption>Normal House</figcaption>
       </figure>
-      <p>
-        <div>
-          level: {level} time: {time} rent: {rent} cost: {cost} inc: {inc}{" "}
-          intId: {intId} auto: {String(auto)} tmp: {tmp}
-        </div>
-        <progress max="6" className="progress-bar"></progress>
-      </p>
-      <p>
-        <button onClick={onBuy}>Buy</button>
-        {Boolean(level) && (
-          <span>
-            <button onClick={onRent}>Rent</button>
-            <button onClick={onManage}>Manage</button>
-          </span>
+      <div>
+        <p>
+          level: {level} time: {time} rent: {rent} cost: {cost} inc: {inc} intId:{" "}
+          {intId} auto: {String(auto)} tmp: {tmp.toFixed(2)}
+        </p>
+        {isRented && (
+          <progress
+            max={time}
+            value={tmp}
+            className="progress-bar"
+          />
         )}
-      </p>
+      </div>
+      {isRented ? (
+        <p>Please Wait !</p>
+      ) : (
+        <p>
+          <button onClick={onBuy}>Buy</button>
+          {Boolean(level) && (
+            <span>
+              <button onClick={onRent}>Rent</button>
+              <button onClick={onManage}>Manage</button>
+            </span>
+          )}
+        </p>
+      )}
     </>
   );
 }
